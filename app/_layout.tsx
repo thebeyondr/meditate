5b8f6811b74b7066c6c0c95336992b341e6ce347
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2024-07-26 16:02:38 -0500

    refactor: :recycle: Update root layout with stacked screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 
-// Import your global CSS file
-// import "../global.css";
-
-export default Slot;
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
